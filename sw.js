65709a8971ba8330317e0b634b0716227ba6294d
UripSW <uriapoo@gmail.com>
2026-08-17 22:04:34 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,7 @@
+const CACHE = 'inventory-app-v2';
+self.addEventListener('install', e => {
+    e.waitUntil(caches.open(CACHE).then(c => c.addAll(['./', './index.html', './manifest.json'])));
+});
+self.addEventListener('fetch', e => {
+    e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
